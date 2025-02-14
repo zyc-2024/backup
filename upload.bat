dfb705f48760768e72b33a74d3b25d33074ebdab
zyc-2024 <61992011@qq.com>
2025-02-14 23:46:23 +0800

    Add files via git

--- upload.bat
+++ upload.bat
@@ -1,10 +1,14 @@
 @echo off
 :f
-echo ----------------------[AutoUpload]commiting----------------------
+echo ----------------------[AutoUpload] Commiting----------------------
 git add -A
 git commit -m "Add files via git"
-echo ---------------------- [AutoUpload]pushing ----------------------
-git push
-@REM timeout /t 5
+echo ---------------------- [AutoUpload] Pushing ----------------------
+if git push; then
+    echo [AutoUpload] Succeed!
+else
+    echo [AutoUpload] Failed!
+    timeout /t 5
+fi
 cls
 goto f
