3a7f5f0baae3a7c38c3f38b771dacdae7727d5b7
zyc-2024 <61992011@qq.com>
2025-02-15 00:20:04 +0800

    E..

--- !upload.bat
+++ !upload.bat
@@ -4,7 +4,6 @@ echo ----------------------[AutoUpload] Commiting----------------------
 git add -A
 git commit -m "Add files via git"
 echo ---------------------- [AutoUpload] Pushing ----------------------
-
 git push | set ifSussess = %1
 if %errorlevel% equ 0 (
     echo [AutoUpload] Success
